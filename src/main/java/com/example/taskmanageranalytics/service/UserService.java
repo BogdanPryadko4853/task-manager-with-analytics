ec5d83570fe980eaa751a1d230d42e15ec9fcaad
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-05-16 15:32:26 +0300

    feat(interface) add UserService

---
+++ src/main/java/com/example/taskmanageranalytics/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.taskmanageranalytics.service;
+
+import com.example.taskmanageranalytics.dto.UserRequest;
+import com.example.taskmanageranalytics.dto.UserResponse;
+import com.example.taskmanageranalytics.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponse createUser(UserRequest userRequest);
+
+    UserResponse getUserById(Long id);
+
+    UserResponse getUserByUsername(String username);
+
+    List<UserResponse> getAllUsers();
+
+    UserResponse updateUser(Long id, UserRequest userRequest);
+
+    void deleteUser(Long id);
+
+    User getRawUserById(Long id);
+}

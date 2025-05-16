fc3f2e5949fef5c75bb9b5601f7db1925d6c7030
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-05-16 15:27:41 +0300

    feat(dto) add UserResponse dto

--- src/main/java/com/example/taskmanageranalytics/dto/UserResponse.java
+++ src/main/java/com/example/taskmanageranalytics/dto/UserResponse.java
@@ -1,4 +1,17 @@
 package com.example.taskmanageranalytics.dto;
 
+import com.example.taskmanageranalytics.entity.User;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private User.Role role;
+    private LocalDateTime createdAt;
+    private Integer createdTasksCount;
+    private Integer assignedTasksCount;
 }

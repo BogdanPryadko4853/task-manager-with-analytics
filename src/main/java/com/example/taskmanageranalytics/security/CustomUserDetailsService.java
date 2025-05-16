a415a769894145f4712b658c55ae59f59646b1ad
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-05-16 23:10:52 +0300

    feat(service) add userDetailsService

---
+++ src/main/java/com/example/taskmanageranalytics/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.taskmanageranalytics.security;
+
+import com.example.taskmanageranalytics.entity.User;
+import com.example.taskmanageranalytics.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("User with username '%s' not found", username)
+                ));
+
+        return new CustomUserDetails(user);
+    }
+}
